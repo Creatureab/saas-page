d7069dfa288f8d552f03da00373240803efa063a
Animesh <animeshbhople885507@gmail.com>
2025-12-17 00:53:34 +0530

    this features commit

--- src/section/Features.tsx
+++ src/section/Features.tsx
@@ -4,8 +4,10 @@ import Featuresbg from "@/assets/Desktop - 8.svg";
 
 const Features = () => {
     return (
-        <section className="py-20 md:py-24">
-            <div className="container">
+        <section className="relative py-20 md:py-24 overflow-hidden">
+            <div className="container relative">
+                <div className="absolute top-[320px] left-1/2 -translate-x-1/2 w-[618px] h-[529px] rounded-full bg-[#5000B5] opacity-[0.23] blur-[150px] pointer-events-none -z-10" />
+
                 <div className="flex justify-center mb-2">
                     <div className="rounded-full p-[1px] bg-gradient-to-r from-[#0560E8] via-[#7000FF] to-[#0560E8]">
                         <div className="flex items-center justify-center px-6 py-2 rounded-full bg-[#030014]">
@@ -17,26 +19,23 @@ const Features = () => {
                 </div>
 
                 <div className="px-4 sm:px-6 lg:px-0">
-                    <h2 className="text-5xl md:text-6xl max-w-2xl font-medium text-[#B6B2FF] tracking-tighter
-                 text-left md:text-center mx-auto">
+                    <h2 className="text-5xl md:text-6xl max-w-2xl font-medium text-[#B6B2FF] tracking-tighter text-left md:text-center mx-auto">
                         Keep track of your meetings all in one place
                     </h2>
-
-                    <p className="mt-6 text-lg text-gray-600 max-w-2xl
-                text-left md:text-center mx-auto">
+                    <p className="mt-6 text-lg text-[#9C99C1] max-w-2xl text-left md:text-center mx-auto">
                         Capture your ideas, thought, and meeting notes in a structured
                         and organized manner.
                     </p>
                 </div>
 
-                <div className="p-2.5 rounded-xl mt-3 flex items-center justify-center">
+                {/* Feature Image */}
+                <div className="relative p-2.5 rounded-xl mt-14 flex items-center justify-center">
                     <Image
                         src={Featuresbg}
-                        alt="Featuresbg"
-                        className="max-w-full h-auto"
+                        alt="Features background"
+                        className="max-w-full h-auto rounded-xl"
                     />
                 </div>
-
             </div>
 
         </section>
